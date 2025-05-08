3a433481b8e91e9f39c6e3fc409c34215f0e2381
EivorKinKest <jmqo2015@gmail.com>
2025-05-08 11:20:26 -0500

    segundo commit

--- ejer.dml.sql
+++ ejer.dml.sql
@@ -1,7 +1,9 @@
+use biblioteca;
+
 INSERT INTO Biblioteca (id_Biblioteca, Nombre) values("1", "LUISA ANGEL ARANGO");
 
-select * from Biblioteca
+select * from Biblioteca;
 
-
+select * from usuario;
 
 
